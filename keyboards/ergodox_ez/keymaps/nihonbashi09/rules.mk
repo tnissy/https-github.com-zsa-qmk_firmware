afbdf72ea393d37f7fae2dd0a8891873fe873965
tnissy <nsgctty@gmail.com>
2021-04-26 20:01:28 +0900

    お久しぶり

--- keyboards/ergodox_ez/keymaps/nihonbashi09/rules.mk
+++
@@ -1,9 +0,0 @@
-# Set any rules.mk overrides for your specific keymap here.
-# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
-LINK_TIME_OPTIMIZATION_ENABLE = yes
-COMMAND_ENABLE = no
-RGBLIGHT_ENABLE = no
-COMBO_ENABLE = yes
-
-# rules.mkでnicola.cをSRCに追加する。
-SRC +=  nicola.c \
